990a6773d6d632570a60ae92210924811d85ecfe
Ayushss-123 <ayushsrivastav323@gmail.com>
2023-02-22 15:44:40 +0000

    Locofy commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,7 @@ import {
   useNavigationType,
   useLocation,
 } from "react-router-dom";
-import Technologies from "../test123/pages/name 123/section 1/Technologies";
-import MAINPAGE from "./public/pages/MAINPAGE";
-import SignUp from "../test123/pages/SignUp";
-import Login from "../test123/pages/Login";
-import ContactUs from "../test123/pages/ContactUs";
-import Component from "../test123/pages/Component";
-import FrameComponent from "./public/pages/FrameComponent";
+import { MyRideBecomeADriverPopu } from "./my-ride-become-a-driver-popu";
 import { useEffect } from "react";
 
 function App() {
@@ -22,38 +16,15 @@ function App() {
     if (action !== "POP") {
       window.scrollTo(0, 0);
     }
-  }, [action, pathname]);
+  }, [action]);
 
   useEffect(() => {
     let title = "";
     let metaDescription = "";
 
+    //TODO: Update meta titles and descriptions below
     switch (pathname) {
       case "/":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/main-page":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/sign-up":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/login":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/contact-us":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/2":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/frame-782":
         title = "";
         metaDescription = "";
         break;
@@ -75,19 +46,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Technologies />} />
-
-      <Route path="/main-page" element={<MAINPAGE />} />
-
-      <Route path="/sign-up" element={<SignUp />} />
-
-      <Route path="/login" element={<Login />} />
-
-      <Route path="/contact-us" element={<ContactUs />} />
-
-      <Route path="/2" element={<Component />} />
-
-      <Route path="/frame-782" element={<FrameComponent />} />
+      <Route path="/" element={<MyRideBecomeADriverPopu />} />
     </Routes>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
@@ -10,22 +10,18 @@ import {
   StyledEngineProvider,
 } from "@mui/material";
 
-import "./global.css";
+const theme = createTheme();
 
-const muiTheme = createTheme();
-
-const container = document.getElementById("root");
-const root = createRoot(container!);
-
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={muiTheme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
     </StyledEngineProvider>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
